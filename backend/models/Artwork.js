936d765f00f3261be7a31e17c0929862513837f6
ahmed oueslati <oueslatiahmed38@gmail.com>
2025-09-19 10:05:56 +0200

    models backend

---
+++ backend/models/Artwork.js
@@ -0,0 +1,68 @@
+import mongoose from 'mongoose';
+
+
+const imageSchema = new mongoose.Schema({
+    url: {
+        type: String,
+        required: true,
+    },
+});
+
+const ArtworkSchema = new mongoose.Schema({
+  auctionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Auction",
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  currency: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: [imageSchema],
+    required: true,
+  },
+  startPrice: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  endPrice: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  status: {
+    type: String,
+    enum: ["live", "draft", "ended", "canceled"],
+    default: "live",
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+});
+
+ArtworkSchema.index({ auctionId: 1, endDate : 1 });
+
+ArtworkSchema.index({ status: 1, endDate: 1, });
+
+
+
+
+export default mongoose.model("Artwork", ArtworkSchema);
+ 
+   
