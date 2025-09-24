5134dc14b62d6d11f454d032be268e1d2c5dc03e
Marvin Wolff <m.w@nr1.io>
2025-09-24 15:57:00 +0200

    init dash

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,19 @@ import Home from "../pages/Home";
 import Auction from "../pages/Auction.jsx";
 import Layout from "../Layout/Layout.jsx";
 import SignUp from "../pages/SignUp.jsx";
-//import CreateAuction from "../pages/CreateAuction.jsx"
-
+import Dashboard from "../pages/Dashboard.jsx";
+// import CreateAuction from "../pages/CreateAuction.jsx"
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="auction" element={<Auction />} />
+        <Route path="dashboard" element={<Dashboard />} />
         <Route path="signup" element={<SignUp />} />
       </Route>
       <Route path="*" element={<h1>Page not found</h1>} />
     </Routes>
   );
 };
-
 export default App;
